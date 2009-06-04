f18b2a300f723d424720c863be283547750e7925
(no author) <(no author)@6e767ac9-f96a-0410-b219-bafc3452b216>
2009-06-04 23:32:53 +0000

    The reflector for instance variables should now be complete.
    git-svn-id: file:///Library/WebStart/SVN/NutShell@59 6e767ac9-f96a-0410-b219-bafc3452b216

--- Cocos/trunk/classes/CSReflectionVariable.m
+++ Cocos/trunk/classes/CSReflectionVariable.m
@@ -12,4 +12,44 @@
 
 @implementation CSReflectionVariable
 
+@synthesize variable;
+@synthesize name;
+@synthesize typeEncoding;
+
++ ( id )reflectorFromVariable:( Ivar )var
+{
+    id reflector = [ [ self alloc ] initWithVariable: var ];
+    
+    return [ reflector autorelease ];
+}
+
+- ( NSString * )description
+{
+    NSString * description;
+    
+    description = [ [ super description ] stringByAppendingFormat: @"\n\t- variable:\t\t%@\n\t- type encoding:\t%@", name, typeEncoding ];
+    
+    return description;
+}
+
+- ( id )initWithVariable:( Ivar )var
+{
+    if( ( self = [ self init ] ) ) {
+        
+        variable     = var;
+        name         = [ [ NSString alloc ] initWithCString: ivar_getName( var ) encoding: NSUTF8StringEncoding ];
+        typeEncoding = [ [ NSString alloc ] initWithCString: ivar_getTypeEncoding( var ) encoding: NSUTF8StringEncoding ];
+    }
+    
+    return self;
+}
+
+- ( void )dealloc
+{
+    [ name release ];
+    [ typeEncoding release ];
+    [ super dealloc ];
+}
+
+
 @end
