a067337251057745524ca1a8ede3993518bdccbf
(no author) <(no author)@6e767ac9-f96a-0410-b219-bafc3452b216>
2009-06-04 00:19:38 +0000

    Added a general header file.
    git-svn-id: file:///Library/WebStart/SVN/NutShell@22 6e767ac9-f96a-0410-b219-bafc3452b216

---
+++ Macmade/trunk/classes/include/Macmade.h
@@ -0,0 +1,10 @@
+// $Id$
+
+// Local includes
+#import "MMEvent.h"
+#import "MMEventDispatcher.h"
+#import "MMLogger.h"
+#import "MMLogMessage.h"
+#import "MMObject.h"
+#import "MMObjectSingleton.h"
+#import "MMSingleton.h"
