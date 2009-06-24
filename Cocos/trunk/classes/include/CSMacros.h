f07cfd39ce7b615a9f328e52c80239bbd7d4d022
(no author) <(no author)@6e767ac9-f96a-0410-b219-bafc3452b216>
2009-06-24 12:01:22 +0000

    Added a macros file.
    git-svn-id: file:///Library/WebStart/SVN/NutShell@132 6e767ac9-f96a-0410-b219-bafc3452b216

---
+++ Cocos/trunk/classes/include/CSMacros.h
@@ -0,0 +1,34 @@
+/*******************************************************************************
+ * Cocos Framework
+ * 
+ * Copyright (c) 2009, Jean-David Gadina (macmade@eosgarden.com)
+ * All rights reserved.
+ ******************************************************************************/
+
+// $Id$
+
+/*!
+ * @header
+ * 
+ * @discussion  This file contains the C preprocessor macros used internally by
+ *              the Cocos Framework. It's not supposed to be public, nor used by
+ *              applications that use the Cocos framework
+ */
+
+#define CSFATAL( ... )                                      \
+    NSException * _csInternalException = [ NSException      \
+        exceptionWithName: @"CSFatalException"              \
+        reason: [ NSString stringWithFormat: __VA_ARGS__ ]  \
+        userInfo: nil                                       \
+    ];                                                      \
+    @throw _csInternalException;
+
+#define CSMALLOC( var, type )                                       \
+    if( NULL == ( var = ( type * )calloc( 1, sizeof( type ) ) ) ) { \
+        CSFATAL( @"Malloc error (%s)\n", strerror( errno ) );       \
+    }
+
+#define CSXMALLOC( var, type, x )                                   \
+    if( NULL == ( var = ( type * )calloc( x, sizeof( type ) ) ) ) { \
+        CSFATAL( @"Malloc error (%s)\n", strerror( errno ) );       \
+    }
