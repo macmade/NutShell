42a784585d093d9845002cc1540619b068b2a240
(no author) <(no author)@6e767ac9-f96a-0410-b219-bafc3452b216>
2009-07-07 13:07:11 +0000

    The URL downloader should now be working.
    git-svn-id: file:///Library/WebStart/SVN/NutShell@162 6e767ac9-f96a-0410-b219-bafc3452b216

--- Cocos/trunk/classes/include/CSURLDownloader.h
+++ Cocos/trunk/classes/include/CSURLDownloader.h
@@ -29,7 +29,85 @@
 {
 @protected
     
+    /*!
+     * @abstract    
+     */
+    BOOL downloading;
     
+    /*!
+     * @abstract    
+     */
+    BOOL downloaded;
+    
+    /*!
+     * @abstract    
+     */
+    BOOL allowOverwrite;
+    
+    /*!
+     * @abstract    
+     */
+    long long bytesTotal;
+    
+    /*!
+     * @abstract    
+     */
+    long long bytesReceived;
+    
+    /*!
+     * @abstract    
+     */
+    long long lastBytesReceived;
+    
+    /*!
+     * @abstract    
+     */
+    double bytesPerSecond;
+    
+    /*!
+     * @abstract    
+     */
+    double percentComplete;
+    
+    /*!
+     * @abstract    
+     */
+    double secondsRemaining;
+    
+    /*!
+     * @abstract    
+     */
+    NSURL * url;
+    
+    /*!
+     * @abstract    
+     */
+    NSString * downloadDirectory;
+    
+    /*!
+     * @abstract    
+     */
+    NSString * downloadLocation;
+    
+    /*!
+     * @abstract    
+     */
+    NSURLDownload * download;
+    
+    /*!
+     * @abstract    
+     */
+    NSURLRequest * request;
+    
+    /*!
+     * @abstract    
+     */
+    NSURLResponse * response;
+    
+    /*!
+     * @abstract    
+     */
+    NSTimer * timer;
     
 @private
     
@@ -49,5 +127,68 @@
      */
     id CS_r2;
 }
+
+/*! @property */
+@property( readonly, getter=isDownloading ) BOOL downloading;
+
+/*! @property */
+@property( readonly, getter=isDownloaded ) BOOL downloaded;
+
+/*! @property */
+@property( readonly ) BOOL allowOverwrite;
+
+/*! @property */
+@property( readonly ) long long bytesTotal;
+
+/*! @property */
+@property( readonly ) long long bytesReceived;
+
+/*! @property */
+@property( readonly ) double bytesPerSecond;
+
+/*! @property */
+@property( readonly ) double percentComplete;
+
+/*! @property */
+@property( readonly ) double secondsRemaining;
+
+/*! @property */
+@property( readonly ) NSURL * url;
+
+/*! @property */
+@property( copy, readwrite ) NSString * downloadDirectory;
+
+/*! @property */
+@property( readonly ) NSString * downloadLocation;
+
+/*!
+ * @abstract    
+ */
++ ( id )downloaderFromURL: ( NSURL * )urlObject;
+
+/*!
+ * @abstract    
+ */
++ ( id )downloaderFromString: ( NSString * )str;
+
+/*!
+ * @abstract    
+ */
+- ( id )initWithURL: ( NSURL * )urlObject;
+
+/*!
+ * @abstract    
+ */
+- ( id )initWithString: ( NSString * )str;
+
+/*!
+ * @abstract    
+ */
+- ( void )start;
+
+/*!
+ * @abstract    
+ */
+- ( void )cancel;
 
 @end
