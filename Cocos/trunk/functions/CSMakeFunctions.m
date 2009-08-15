458c2e0bd7751da7e02fee411fc208094e52da57
(no author) <(no author)@6e767ac9-f96a-0410-b219-bafc3452b216>
2009-08-15 02:52:21 +0000

    Added a function file for functions that create objects or structs.
    git-svn-id: file:///Library/WebStart/SVN/NutShell@204 6e767ac9-f96a-0410-b219-bafc3452b216

---
+++ Cocos/trunk/functions/CSMakeFunctions.m
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Cocos Framework
+ * 
+ * Copyright (c) 2009, Jean-David Gadina (macmade@eosgarden.com)
+ * All rights reserved.
+ ******************************************************************************/
+
+// $Id$
+
+#import "CSMakeFunctions.h"
+
+NSColor * CSMakeColor( NSUInteger v1, NSUInteger v2, NSUInteger v3, NSUInteger alpha, CSColorType type )
+{
+    switch( type ) {
+        
+        case CSColorTypeHSB:
+            
+            return [ NSColor colorWithCalibratedHue: ( CGFloat )v1 / 360 saturation: ( CGFloat )v2 / 100 brightness: ( CGFloat )v3 / 100 alpha: ( CGFloat )alpha / 100 ];
+            
+        default:
+            
+            return [ NSColor colorWithCalibratedRed: ( CGFloat )v1 / 255 green: ( CGFloat )v2 / 255 blue: ( CGFloat )v3 / 255 alpha: ( CGFloat )alpha / 100 ];
+    }
+}
